e41ab4734b60d2f316cf4d044857195109f5279b
arun <40155193+just-arun@users.noreply.github.com>
2019-06-19 08:31:25 +0530

    Update keys.js

--- api/config/keys.js
+++ api/config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoURI: 'mongodb://localhost/test',
-    smsHASH: '7800561a4110134357f8d646d296775d942547479d55ed147c4bd00fbb444277',
-    mname: "thequeuedev@gmail.com",
-    mpwd: "thisisdevshit@2"
+    smsHASH: '',
+    mname: "",
+    mpwd: ""
 }
